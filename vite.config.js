3305029fb345589a76b1a284d54dae701fc8cddd
Vikrant Singh <142868546+creativeriser@users.noreply.github.com>
2025-04-25 14:12:05 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        scholarships: 'scholarships.html',
+        kvpyApplication: 'kvpy-application.html'
+      }
+    },
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
+});
